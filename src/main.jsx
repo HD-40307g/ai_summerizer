784cf248bc987c40c51c33ad6a48999b63fd0fb6
HD-40307g <152268341+HD-40307g@users.noreply.github.com>
2024-10-06 17:12:53 +0200

    Add Hero.jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import ReactDom from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = createRoot(document.getElementById('root'))
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
